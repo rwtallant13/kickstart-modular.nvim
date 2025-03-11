07985d3737d45591b0076307dd5e17e799e3d121
Robert Tallant <rwtallant13@gmail.com>
2025-03-11 14:48:47 -0400

    Figuring shit out slowly

---
+++ lua/custom/plugins/motions.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    'ggandor/leap.nvim',
+    dependencies = { 'tpope/vim-repeat' },
+  },
+
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+  },
+}
---
+++ lua/custom/plugins/util.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    'luukvbaal/nnn.nvim',
+    opts = {},
+  },
+
+  {
+    'gbprod/yanky.nvim',
+    event = 'VeryLazy',
+    opts = {
+      vim.keymap.set({ 'n', 'x' }, 'p', '<Plug>(YankyPutAfter)'),
+      vim.keymap.set({ 'n', 'x' }, 'P', '<Plug>(YankyPutBefore)'),
+      vim.keymap.set({ 'n', 'x' }, 'gp', '<Plug>(YankyGPutAfter)'),
+      vim.keymap.set({ 'n', 'x' }, 'gP', '<Plug>(YankyGPutBefore)'),
+      vim.keymap.set('n', '<c-p>', '<Plug>(YankyPreviousEntry)'),
+      vim.keymap.set('n', '<c-n>', '<Plug>(YankyNextEntry)'),
+    },
+  },
+
+  {
+    'AckslD/nvim-neoclip.lua',
+    dependencies = {
+      -- you'll need at least one of these
+      { 'nvim-telescope/telescope.nvim' },
+      -- {'ibhagwan/fzf-lua'},
+    },
+    opts = {},
+  },
+
+  {
+    'jghauser/mkdir.nvim',
+  },
+}
