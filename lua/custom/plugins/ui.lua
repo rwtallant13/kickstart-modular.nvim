4b29b449c016487bc54699b0c5b22ca2c0ae6a50
Robert Tallant <rwtallant13@gmail.com>
2025-03-12 21:26:32 -0400

    floatingterm.lua

--- lua/custom/plugins/ui.lua
+++ lua/custom/plugins/ui.lua
@@ -8,7 +8,7 @@ return {
       scope = { enabled = true },
       scroll = { enabled = true },
       dashboard = { enabled = true },
-      statuscolumn = { enabled = false }, -- we set this in options.lua
+      statuscolumn = { enabled = false },
       toggle = {},
       words = { enabled = true },
       zen = { enabled = true },
@@ -57,11 +57,8 @@ return {
     priority = 1000,
     init = function()
       -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       vim.cmd.colorscheme 'catppuccin'
 
-      -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
   },
@@ -98,20 +95,17 @@ return {
     },
   -- stylua: ignore
   keys = {
-    { "<leader>sn", "", desc = "+noice"},
+    { "<leader>sN", "", desc = "+noice"},
     { "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline" },
-    { "<leader>snl", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
-    { "<leader>snh", function() require("noice").cmd("history") end, desc = "Noice History" },
-    { "<leader>sna", function() require("noice").cmd("all") end, desc = "Noice All" },
-    { "<leader>snd", function() require("noice").cmd("dismiss") end, desc = "Dismiss All" },
-    { "<leader>snt", function() require("noice").cmd("pick") end, desc = "Noice Picker (Telescope/FzfLua)" },
+    { "<leader>sNl", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
+    { "<leader>sNh", function() require("noice").cmd("history") end, desc = "Noice History" },
+    { "<leader>sNa", function() require("noice").cmd("all") end, desc = "Noice All" },
+    { "<leader>sNd", function() require("noice").cmd("dismiss") end, desc = "Dismiss All" },
+    { "<leader>sNt", function() require("noice").cmd("pick") end, desc = "Noice Picker (Telescope/FzfLua)" },
     { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll Forward", mode = {"i", "n", "s"} },
     { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll Backward", mode = {"i", "n", "s"}},
   },
     config = function(_, opts)
-      -- HACK: noice shows messages from before it was enabled,
-      -- but this is not ideal when Lazy is installing plugins,
-      -- so clear the messages in this case.
       if vim.o.filetype == 'lazy' then
         vim.cmd [[messages clear]]
       end
