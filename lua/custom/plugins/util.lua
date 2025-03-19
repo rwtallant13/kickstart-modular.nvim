53344a63bd13f43acd85e8a1a58c202526fada09
Robert Tallant <rwtallant13@gmail.com>
2025-03-19 13:31:25 -0400

    fixing keybinds

--- lua/custom/plugins/util.lua
+++ lua/custom/plugins/util.lua
@@ -18,19 +18,6 @@ return {
   },
 
   {
-    'gbprod/yanky.nvim',
-    event = 'VeryLazy',
-    opts = {
-      vim.keymap.set({ 'n', 'x' }, 'p', '<Plug>(YankyPutAfter)'),
-      vim.keymap.set({ 'n', 'x' }, 'P', '<Plug>(YankyPutBefore)'),
-      vim.keymap.set({ 'n', 'x' }, 'gp', '<Plug>(YankyGPutAfter)'),
-      vim.keymap.set({ 'n', 'x' }, 'gP', '<Plug>(YankyGPutBefore)'),
-      vim.keymap.set('n', '<c-p>', '<Plug>(YankyPreviousEntry)'),
-      vim.keymap.set('n', '<c-n>', '<Plug>(YankyNextEntry)'),
-    },
-  },
-
-  {
     'AckslD/nvim-neoclip.lua',
     dependencies = {
       { 'kkharji/sqlite.lua', module = 'sqlite' },
--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -197,8 +197,8 @@ return {
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         bashls = {},
-        docker_compose_language_service = {},
-        yamlls = {},
+        -- docker_compose_language_service = {},
+        -- yamlls = {},
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
